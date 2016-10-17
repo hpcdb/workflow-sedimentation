d61bd01f7f2f18c8f2dd43c33f9d7d4120c3cc41
Vítor Silva <silva@cos.ufrj.br>
2016-10-17 17:10:05 -0200

    Bugs to execute in Mac OS (Sierra).

--- sedimentation/execute-solver.sh
+++ sedimentation/execute-solver.sh
@@ -3,6 +3,7 @@
 
 # mac
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/vitor/Documents/Program_Installations/ParaView-v5.1.2/build/CMakeFiles/__macos_install/lib/paraview-5.1
+# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/vitor/Documents/Program_Installations/ParaView-v5.1.2/build/CMakeFiles/__macos_install/lib/paraview-5.1:/usr/local/opt/gcc/lib/gcc/6/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/vitor/Documents/Program_Installations/ParaView-v5.1.2/build/lib/paraview-5.1
 
 # virtual box - mint
