ae28893b87dd685b624ee30a199fa8343566c556
Vítor Silva <silva@cos.ufrj.br>
2017-01-02 14:58:55 -0200

    LoboC configuration.

--- loboc/sedimentation/exp-idx-ofb/delete.sh
+++ loboc/sedimentation/exp-idx-ofb/delete.sh
@@ -5,11 +5,14 @@ rm -rf out*
 rm temp*
 rm *.csv
 rm -rf prov/log/*
+rm -rf prov/paraview/*
 rm -rf prov/rde/*
+rm -rf prov/rdi/*
 rm -rf prov/solver/*
 rm -rf prov/pg/*
 rm -rf prov/di/*
 rm -rf prov/indexing/*
+rm -rf prov/visualization/*
 rm -rf log.*.out
 rm -rf log.*.err
 rm -rf *.conf
@@ -28,14 +31,16 @@ rm -rf out.txt
 rm -rf sed-ext*
 rm -rf image_*.png
 rm -rf video.mp4
-rm -rf mpirun.sh
 mkdir prov
 mkdir prov/di
 mkdir prov/pg
 mkdir prov/log
+mkdir prov/paraview
 mkdir prov/rde
+mkdir prov/rdi
 mkdir prov/solver
 mkdir prov/indexing
+mkdir prov/visualization
 mkdir prov/pg/sedimentation
 mkdir prov/di/sedimentation
 cp dataflow.json prov/pg/sedimentation/
