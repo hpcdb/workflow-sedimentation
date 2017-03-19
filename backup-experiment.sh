06fcd5f52dbcd9327db20135e55490d56aaa1610
vssousa <vitor.silva.sousa@gmail.com>
2017-03-19 20:33:35 -0300

    Creating a script to back up experiment files.

---
+++ backup-experiment.sh
@@ -0,0 +1,10 @@
+# zip -r index.zip index
+# zip -r indexing.zip *.index
+
+zip -r data.zip data
+zip -r output.zip output
+zip -r prov.zip prov
+zip -r extraction.zip *.csv
+zip -r visualization.zip *.png
+
+
