a7b06c2b99415c50158e6b2e3b60e5e9836b1c67
Vítor Silva <vitor.silva.sousa@gmail.com>
2017-03-27 10:18:42 -0300

    Update.

--- src-local/sedimentation/execute-solver.sh
+++ src-local/sedimentation/execute-solver.sh
@@ -10,8 +10,12 @@
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/src/paraview/lib/paraview-5.1
 
 # Mint
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/Documents/program/paraview/lib/paraview-5.1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/Documents/program/paraview/lib/paraview-5.1
+#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/Documents/program/paraview/lib/paraview-5.1
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/Documents/program/paraview/lib/paraview-5.1
+
+# Mint - NACAD
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/program/paraview-5.3.0/lib/paraview-5.3
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/program/paraview-5.3.0/lib/paraview-5.3
 
 # stampede
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/03664/silva/programs/paraview/lib/paraview-5.0
@@ -25,7 +29,7 @@ rm output-statistics.log
 # time mpirun -np 2 ../libmesh-sedimentation/libmesh-sedimentation-opt analysis2D.py | tee -a "output-solver.log"
 #time mpirun -np 2 ../libmesh-sedimentation/libmesh-sedimentation-opt analysis3D.py | tee -a "output-solver.log"
 
-time mpirun -np 4 ../../libmesh-sedimentation/sediment-opt -i sedimentation.in -m necker3d.msh -e extraction.py -v visualization.py -o output -d /home/vitor/Documents/dev/workflow-sedimentation/src-local/sedimentation/output/ | tee -a "output-solver.log"
+time mpirun -np 2 ../../libmesh-sedimentation/sediment-opt -i sedimentation.in -m necker3d.msh -e extraction.py -v visualization.py -o output -d /home/vitor/Documents/dev/workflow-sedimentation/src-local/sedimentation/output/ | tee -a "output-solver.log"
 
 #time ../libmesh-sedimentation/libmesh-sedimentation-opt
 # Calculate total elapsed time for provenance gathering
