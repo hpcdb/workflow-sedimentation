8b083d2bf7a6b83d0ab84729472153dc04e469e4
Vítor Silva <vitor.silva.sousa@gmail.com>
2017-07-02 17:47:54 -0300

    Updating files to run libMesh-sedimentation solver on the new laptop (inspiron)

--- src-local/sedimentation/execute-solver.sh
+++ src-local/sedimentation/execute-solver.sh
@@ -2,8 +2,8 @@
 # solver execution
 
 # mac
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/vitor/Documents/program/paraview-5.4.0/CMakeFiles/__macos_install/lib/paraview-5.4:/usr/local/opt/gcc/lib/gcc/6/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/vitor/Documents/program/paraview-5.4.0/CMakeFiles/__macos_install/lib/paraview-5.4
+#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/vitor/Documents/program/paraview-5.4.0/CMakeFiles/__macos_install/lib/paraview-5.4:/usr/local/opt/gcc/lib/gcc/6/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/vitor/Documents/program/paraview-5.4.0/CMakeFiles/__macos_install/lib/paraview-5.4
 
 # virtual box - mint
 # export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/src/paraview/lib/paraview-5.1
@@ -25,6 +25,10 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Users/vitor/Documents/program/paraview-
 # export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/program/paraview-5.3.0/lib/paraview-5.3
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/program/paraview-5.3.0/lib/paraview-5.3
 
+# Ubuntu - Inspiron
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/home/vitor/Documents/program/paraview-v5.4.0/lib/paraview-5.4
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vitor/Documents/program/paraview-v5.4.0/lib/paraview-5.4
+
 # stampede
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/03664/silva/programs/paraview/lib/paraview-5.0
 
@@ -43,6 +47,7 @@ rm output-statistics.log
 # time mpirun -np 2 ../../libmesh-sedimentation/sediment-opt -i lock_necker3D_pc11.in -m lock_necker3D.msh -e extraction.py -v visualization.py -o output -d /home/vitor/Documents/dev/workflow-sedimentation/src-local/sedimentation/output | tee -a "output-solver.log"
 
 # MacOS
-time mpirun -np 2 ../../libmesh-sedimentation/sediment-opt -i lock_necker3D_pc11.in -m lock_necker3D.msh -e extraction.py -v visualization.py -o output -d /Users/vitor/Documents/repository/workflow-sedimentation/src-local/sedimentation/output | tee -a "output-solver.log"
+# time mpirun -np 2 ../../libmesh-sedimentation/sediment-opt -i lock_necker3D_pc11.in -m lock_necker3D.msh -e extraction.py -v visualization.py -o output -d /Users/vitor/Documents/repository/workflow-sedimentation/src-local/sedimentation/output | tee -a "output-solver.log"
 
-
+# Ubuntu - Inspiron
+time mpirun -np 2 ../../libmesh-sedimentation/sediment-opt -i lock_necker3D_pc11.in -m lock_necker3D.msh -e extraction.py -v visualization.py -o output -d /home/vitor/Documents/dev/workflow-sedimentation/src-local/sedimentation/output | tee -a "output-solver.log"
